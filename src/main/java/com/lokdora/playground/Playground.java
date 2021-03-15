f85da54132e7b4d0a9d9ef2d50932fdba8541a61
Dorapower <32120829+Dorapower@users.noreply.github.com>
2021-03-16 01:42:16 +0800

    setup the environment

---
+++ src/main/java/com/lokdora/playground/Playground.java
@@ -0,0 +1,10 @@
+package com.lokdora.playground;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Playground implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
